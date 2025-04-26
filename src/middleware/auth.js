f0ece6bed172ce4939e9cae25b4d690a08b31210
Oluwole <oluwoleafolabi0@gmail.com>
2025-04-26 23:06:37 +0100

    refactor: add extra validation

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,6 +4,7 @@ const { AppError } = require('./errorHandler');
 module.exports = async (req, res, next) => {
     try {
         let token;
+        
         if (req.headers.authorization?.startsWith('Bearer')) {
             token = req.headers.authorization.split(' ')[1];
         }
@@ -13,6 +14,23 @@ module.exports = async (req, res, next) => {
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await User.findUnique({
+            where: { id: decoded.id },
+            select: { password_changed_at: true }
+        });
+
+        if (!user) {
+            return next(new AppError('This user does not exist or has been deleted', 401));
+        }
+
+        if (user.password_changed_at) {
+            const changedTimeStamp = parseInt(user.password_changed_at.getTime() / 1000, 10);
+            if (changedTimeStamp > decoded.iat) {
+                return next(new AppError('User recently changed password. Please log in again', 401));
+            }
+        }
+
         req.user = decoded;
         next();
     } catch (error) {
