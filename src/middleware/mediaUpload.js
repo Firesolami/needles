9d4a2cadc830200818e537b8fff9289e3cd84099
Oluwole <oluwoleafolabi0@gmail.com>
2025-04-27 14:55:24 +0100

    feat: add multer media upload middleware

---
+++ src/middleware/mediaUpload.js
@@ -0,0 +1,34 @@
+const multer = require('multer');
+const { AppError } = require('./errorHandler');
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+    const allowedTypes = [
+        'audio/',
+        'video/',
+        'image/',
+    ];
+
+    if (allowedTypes.includes(file.mimetype.split('/')[0])) {
+        cb(null, true);
+    } else {
+        cb(
+            new AppError(
+                'Invalid file type. Only audio, video and image files are allowed.',
+                400
+            ),
+            false
+        );
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 10 * 1024 * 1024
+    }
+});
+
+module.exports = upload;
